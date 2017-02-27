ad5eb886d309a8722bfa0a668db6b3524cfed90f
lucymonie <lucy.monie.hall@gmail.com>
2017-02-27 11:30:07 +0000

    Adds files for testing exercise

---
+++ gds-pair-exercise/main.js
@@ -0,0 +1,3 @@
+var test = function (name) {
+  return 'Hello ' + name + '!';
+}
---
+++ gds-pair-exercise/main.test.js
@@ -0,0 +1,6 @@
+describe("Tests for gds pair interview", function() {
+  it('should validate the test suite is working correctly', function() {
+    expect(test('world')).toBe('Hello world!');
+  });
+
+})
