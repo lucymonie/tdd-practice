0a8a143c55a67f68d2cf37ae6809ec50d65ad0a9
lucymonie <lucy.monie.hall@gmail.com>
2017-03-04 10:58:47 +0000

    Adds passing tests for constructor function

--- pair-coding/main.js
+++ pair-coding/main.js
@@ -1,3 +1,27 @@
 var test = function (name) {
   return 'Hello ' + name + '!';
 }
+
+const Checkout = function () {
+  this.sum = 0;
+  this.tracking = {
+    FR1: 0,
+    SR1: 0,
+    CF1: 0
+  }
+  this.scan = function (productCode) {
+    this.tracking.productCode += 1
+  }
+  this.total = function() {
+    return sum;
+  }
+}
+
+let co = new Checkout();
+
+
+const items = {
+	FR1: {name: 'Fruit tea', price: 3.11, discount: 'two for one'},
+	SR1: {name: 'Strawberries', price: 5.00, discount: 'half price'},
+	CF1: {name: 'Coffee', price: 11.23, discount: null}
+}
--- pair-coding/main.test.js
+++ pair-coding/main.test.js
@@ -1,6 +1,19 @@
-describe("Tests for gds pair interview", function() {
-  it('should validate the test suite is working correctly', function() {
-    expect(test('world')).toBe('Hello world!');
-  });
+describe("Tests for checkout", function() {
+  it('should create a new instance of checkout when constructor is called', function () {
+    let co = new Checkout()
+    expect(typeof co).toBe('object');
+    expect(typeof co.scan).toBe('function');
+  })
+
+  // it('should check that co.scan picks up the correct item', function() {
+  //   co.scan(items.FR1);
+  //   expect(co.total(sum)).toBe(3.11);
+  // });
+
+  // it('should check that co.total returns the current total', function() {
+  //   sum = 0;
+  //   checkout.scan(items.FR1);
+  //   expect(checkout.total(sum)).toBe(3.11);
+  // });
 
 })
