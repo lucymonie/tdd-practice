c2877f0db3ed85951231c907fa300c5d62c5bf94
lucymonie <lucy.monie.hall@gmail.com>
2017-03-12 09:21:12 +0000

    Refactor main.js to have separate Checkout.prototype methods

--- pair-coding/main.js
+++ pair-coding/main.js
@@ -4,23 +4,26 @@ const Checkout = function () {
     SR1: 0,
     CF1: 0
   };
-  this.scan = function (productCode) {
-    this.basket[productCode] += 1;
-    this.subtotal += items[productCode].price;
-  };
   this.subtotal = 0;
-  this.discount = function () {
-    let discountTotal = this.subtotal;
-    let discountNumFr1 = Math.ceil(this.basket.FR1 / 2);
-     discountTotal -= (items.FR1.price * this.basket.FR1);
-     discountTotal += (items.FR1.price * discountNumFr1);
-     discountTotal -= ((items.SR1.price / 2) * this.basket.SR1);
-    return discountTotal;
-  }
-  this.total = function() {
-    return this.discount();
-  };
 }
+
+Checkout.prototype.scan = function (productCode) {
+  this.basket[productCode] += 1;
+  this.subtotal += items[productCode].price;
+};
+
+Checkout.prototype.discount = function () {
+  let discountTotal = this.subtotal;
+  let discountNumFr1 = Math.ceil(this.basket.FR1 / 2);
+   discountTotal -= (items.FR1.price * this.basket.FR1);
+   discountTotal += (items.FR1.price * discountNumFr1);
+   discountTotal -= ((items.SR1.price / 2) * this.basket.SR1);
+  return discountTotal;
+}
+
+Checkout.prototype.total = function() {
+  return this.discount();
+};
 
 const items = {
 	FR1: {name: 'Fruit tea', price: 3.11, discount: 'two for one'},
